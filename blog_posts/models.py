959b13663413db2206bd5d1fdff0c01b14118203
Daniel <3138740851@qq.com>
2020-12-23 18:20:47 -0500

    add migrations

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model): # each model inherits from the database
+  title = models.CharField(max_length=100) # charfield is varchar
+  content = models.TextField() # text field is unrestricted text
+  date_posted = models.DateTimeField(default=timezone.now)
+  author = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+  def __str__(self):
+    return self.title
--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post # use . if in the same directory
 
-posts = [
-  {
-    'author': 'DanielS',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'December 23rd, 2020'
-  },
-  {
-    'author': 'Dude',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'December 24th, 2020'
-  }
-]
-# Create your views here.
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'blog_posts/home.html', context) # this is still using a httpresponse
 
