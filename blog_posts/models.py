6bb0bfd231722d75828fa830bc45ce7756f22bd0
Daniel <3138740851@qq.com>
2020-12-28 14:47:02 -0500

    add update and delete functionality

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model): # each model inherits from the database
@@ -11,3 +12,6 @@ class Post(models.Model): # each model inherits from the database
 
   def __str__(self):
     return self.title
+  
+  def get_absolute_url(self):
+    return reverse('post-detail', kwargs={'pk': self.pk})
--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, 
+                                  DetailView, 
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 from django.http import HttpResponse
 from .models import Post # use . if in the same directory
 
@@ -18,6 +23,42 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
   model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+  model = Post
+  success_url = "/"
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
 
 def about(request):
   return render(request, 'blog_posts/about.html', {'title': 'About Page'})
