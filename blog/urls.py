7a83898408b071f8b25f61b1ee858bc4ec5f63fa
Daniel <3138740851@qq.com>
2020-12-28 09:02:03 -0500

    add form validation for registration

--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,6 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/register', user_views.register, name="register"),
+    path('register/', user_views.register, name="register"),
     path('', include('blog_posts.urls')) #routes are similar to rails
 ]
