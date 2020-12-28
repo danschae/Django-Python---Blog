e1533294a64c035b8e7103d9a35bd15432221085
Daniel <3138740851@qq.com>
2020-12-28 13:47:35 -0500

    add edit profile page and upload a new profile pic functionality

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
   email = forms.EmailField() # default is require is true
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm):
   class Meta: # meta is used to add configurations to the form, any information that is not a form field such as model, fields, exclude and widgets can be used
     model = User
     fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+  email = forms.EmailField() # default is require is true
+
+  class Meta: 
+    model = User
+    fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    fields = ['image']
