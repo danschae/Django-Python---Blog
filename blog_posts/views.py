57258001d317fba5d10a7c275be7b0451e0aae1d
Daniel <3138740851@qq.com>
2020-12-28 14:12:49 -0500

    switch views to class base/ add individual post page

--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Post # use . if in the same directory
 
-def home(request):
-  context = {
-    'posts': Post.objects.all()
-  }
-  return render(request, 'blog_posts/home.html', context) # this is still using a httpresponse
+# def home(request):
+#   context = {
+#     'posts': Post.objects.all()
+#   }
+#   return render(request, 'blog_posts/home.html', context) # this is still using a httpresponse
+
+class PostListView(ListView):
+  model = Post
+  # looks for template at <app>/<model>_<viewtype>.html
+  template_name = 'blog_posts/home.html'
+  context_object_name = 'posts'
+  ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+  model = Post
 
 def about(request):
   return render(request, 'blog_posts/about.html', {'title': 'About Page'})
